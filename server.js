4ce1510db1d66daadc3df09e5990e20649503ccd
Yatishhr <yatishhr@gmail.com>
2017-08-23 20:55:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/ui/main.js',function(req,res){
     res.sendFile(path.join(__dirname,'ui','main.js'));
 });
-app.get('/',function(req,res){
+app.get('/new',function(req,res){
     res.sendFile(path.join('new.html'));
 });
 app.get('/ui/main.js',function(req,res){
