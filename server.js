7f2ac4d7f26b8c6569be57d1cb9df54fc42cfe3e
Yatishhr <yatishhr@gmail.com>
2017-08-23 20:57:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/ui/main.js',function(req,res){
     res.sendFile(path.join(__dirname,'ui','main.js'));
 });
 app.get('/new',function(req,res){
-    res.sendFile(path.join('new.html'));
+    res.sendFile('/new.html');
 });
 app.get('/ui/main.js',function(req,res){
     res.sendFile(path.join(__dirname,'ui','main.js'));
