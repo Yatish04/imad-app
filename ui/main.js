3263a382788a68e6b965ccdbcd740835796f95e9
Yatishhr <yatishhr@gmail.com>
2017-08-23 22:38:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,14 +3,19 @@ var button=document.getElementById('btn1');
 
 button.onclick=function() {
     var request= new XMLHttpRequest();
+        request.open('GET','http://yatishhr.imad.hasura-app.io/new',true);
+    request.send(null);
    request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200)
             {
                alert('hello'); 
             }
+            else{
+                alert('wrong');
+            }
         }
     }
-    request.open('GET','http://yatishhr.imad.hasura-app.io/new',true);
+
 }
 }
