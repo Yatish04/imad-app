4eae87cd396ba11167084d76ba3c70459412fc0f
Yatishhr <yatishhr@gmail.com>
2017-08-24 20:34:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,17 +26,16 @@ function fun1(){
     window.open("http://yatishhr.imad.hasura-app.io/new", "_self");
 }
 function btn(){
+    var request=new XMLHttpRequest();
     var uname=document.getElementById('username');
     var passwrd=document.getElementById('password');
     var name=document.getElementById('name');
     var email=document.getElementById('email');
-    var request=new XMLHttpRequest();
     request.open("POST","http://yatishhr.imad.hasura-app.io/newuser",true);
    request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200)
             {
-                request.send(JSON.stringify({name:name,username:username,email:email,password:password}));
                 window.open('GET','http://yatishhr.imad.hasura-app.io/login',true);
                 alert('Registered successfully');
             }
@@ -46,4 +45,6 @@ function btn(){
             }
         }
    };
+request.send(JSON.stringify({name:name,username:username,email:email,password:password}));
+
 }
