b4fd3c896b8b8009ca02cc67c4ad9a271d70f503
Yatishhr <yatishhr@gmail.com>
2017-08-25 06:37:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,9 +47,9 @@ function hash(password,salt){
     return ["pbkdf2","1000",salt,hashed.toString('hex')].join('$');
 }
 
-app.post('/createuser',function(req,res){
-    var username=req.body.username;
-    var password=req.body.password;
+app.get('/createuser',function(req,res){
+    var username="hello";
+    var password="hello";
     var salt=crypto.randomBytes(128).toString('hex');
     var dbstring=hash(password,salt);
     pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,password],function(err,res){
