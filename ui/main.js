7e66e26100f6e644dfe9a80d412fede535e6dd20
Yatishhr <yatishhr@gmail.com>
2017-08-25 07:46:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,8 +35,7 @@ function btn(){
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200)
             {
-                window.open('GET','http://yatishhr.imad.hasura-app.io/login',true);
-                //alert('Registered successfully');
+                alert('Registered successfully \n Go back and login');
             }
             else{
                 alert('Something went wrong');
@@ -46,7 +45,9 @@ function btn(){
    request.open('POST','http://yatishhr.imad.hasura-app.io/newuser',true);
    request.setRequestHeader('Content-Type','application/json');
 request.send(JSON.stringify({username:username,password:password}));
-
+if(request.status===200){
+    window.open('GET','http://yatishhr.imad.hasura-app.io/login',true);
+}
 }
 
 
