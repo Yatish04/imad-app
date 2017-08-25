e1eaf744a235bb8951daf2f33756ae724dcaff7d
Yatishhr <yatishhr@gmail.com>
2017-08-25 07:00:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,8 +61,8 @@ app.get('/createuser',function(req,res){
 });
 
 app.post('/newuser',function(req,res){
-    var username="hello";
-    var password="hello";
+    var username=req.body.username;
+    var password=req.body.password;
         var salt=crypto.randomBytes(128).toString('hex');
     var hashed=hash(password,salt);
     pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,hashed],function(err,result){
